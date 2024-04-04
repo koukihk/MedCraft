691bd4d3fb7cd914f8d559600aae85c6c1946877
koukihk <450012372@qq.com>
2024-04-04 14:10:55 +0800

    4Apr

--- tumor_analyzer.py
+++ tumor_analyzer.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.mixture import GaussianMixture
 from scipy import ndimage
 import nibabel as nib
+from multiprocessing import Pool
+
 
 class TumorAnalyzer:
     def __init__(self):
@@ -12,41 +14,58 @@ class TumorAnalyzer:
         self.indices_to_skip = [32, 34, 38, 41, 47, 87, 89, 91, 105, 106, 114, 115, 119]
 
     def fit_gmm_model(self, data, optimal_components):
+        """
+        Fits a Gaussian Mixture Model to the given data.
+        """
         self.gmm_model = GaussianMixture(n_components=optimal_components, covariance_type='full', init_params='random', tol=0.001, max_iter=100)
         self.gmm_model.fit(data)
 
     def load_data(self, data_folder):
+        """
+        Loads CT scan images and corresponding tumor labels from the specified data folder.
+        """
         ct_files = sorted(os.listdir(os.path.join(data_folder, "img")))
         tumor_positions = []
 
-        for ct_file in ct_files:
+        def process_file(ct_file):
             if ct_file.startswith("._"):
-                continue
+                return []
+
             img_path = os.path.join(data_folder, "img", ct_file)
             label_path = os.path.join(data_folder, "label", ct_file)
 
             file_index = int(ct_file.split('_')[1].split('.')[0])
             if file_index in self.indices_to_skip:
-                continue
+                return []
 
             if not (os.path.isfile(img_path) and os.path.isfile(label_path)):
-                continue
+                return []
 
             label_data = nib.load(label_path).get_fdata()
             positions = self.analyze_tumor_location(label_data, tumor_label=2)
 
-            if positions:
-                tumor_positions.extend(positions)
+            return positions
+
+        with Pool() as pool:
+            results = pool.map(process_file, ct_files)
+
+        tumor_positions = [position for sublist in results for position in sublist]
 
         self.all_tumor_positions = np.array(tumor_positions)
 
     def load_data_and_fit_gmm(self, data_folder, optimal_components):
+        """
+        Loads data and fits GMM model.
+        """
         if not self.has_fitted_gmm:
             self.load_data(data_folder)
             self.fit_gmm_model(self.all_tumor_positions, optimal_components)
             self.has_fitted_gmm = True
 
     def analyze_tumor_location(self, label_data, tumor_label=2):
+        """
+        Analyzes tumor location from label data.
+        """
         labeled_components, num_components = ndimage.label(label_data == tumor_label)
         tumor_positions = []
 
@@ -59,7 +78,13 @@ class TumorAnalyzer:
         return tumor_positions
 
     def get_all_tumor_positions(self):
+        """
+        Returns all tumor positions.
+        """
         return self.all_tumor_positions
 
     def get_gmm_model(self):
+        """
+        Returns the trained GMM model.
+        """
         return self.gmm_model
