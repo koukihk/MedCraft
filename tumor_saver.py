44d37824ff558495484f049672cc739a33c0979c
koukihk <450012372@qq.com>
2024-04-10 21:05:22 +0800

    10Apr4

--- tumor_saver.py
+++ tumor_saver.py
@@ -16,41 +16,28 @@ class TumorSaver:
         return datatype_map.get(datatype.item(), 'uint8')
 
     @staticmethod
-    def get_new_filename(existing_files, base_filename):
-        base, ext = os.path.splitext(base_filename)
-        pattern = re.compile(r'^(\d+_)?(\d+)_\w*\.nii\.gz$')
-        counts = []
-
-        for file in existing_files:
-            match = pattern.match(file)
-            if match:
-                count = int(match.group(2))
-                counts.append(count)
-
-        if not counts:
-            return f"synt_{base_filename}"  # No conflicts, add 'synt_' prefix
-
-        max_count = max(counts)
-        new_count = max_count + 1
-
-        new_filename = f"synt_{new_count}_{base}{ext}"  # Add 'synt_' prefix and numbering
-        return new_filename
-
-    @staticmethod
     def save_nifti(data, affine_matrix, data_type, output_path, filename):
-        if os.path.exists(output_path):
-            existing_files = os.listdir(output_path)
-            new_filename = TumorSaver.get_new_filename(existing_files, filename)
-        else:
-            os.makedirs(output_path, exist_ok=True)
-            new_filename = f"synt_{filename}"
+        os.makedirs(output_path, exist_ok=True)
 
-        output_file = os.path.join(output_path, new_filename)
+        filename = 'synt_' + filename
+        full_path = os.path.join(output_path, filename)
+
+        if os.path.exists(full_path):
+            counter = 1
+            while os.path.exists(os.path.join(output_path, filename)):
+                filename, _ = os.path.splitext(filename)
+                filename, _ = os.path.splitext(filename)
+                filename = f"{filename}_{counter}"
+                counter += 1
+
+            filename += '.nii.gz'
+            full_path = os.path.join(output_path, filename)
+
         nib.save(
             nib.Nifti1Image(data.astype(data_type), affine_matrix),
-            output_file
+            full_path
         )
-        print(f"Saved {output_file}")
+        print(f"Saved {full_path}")
 
     @staticmethod
     def save_data(d, folder='global'):
@@ -63,11 +50,12 @@ class TumorSaver:
         image = d['image'][0].squeeze(0).cpu().numpy()
         label = d['label'][0].squeeze(0).cpu().numpy()
 
-        image_outputs = f'synt/{folder}/image'
-        label_outputs = f'synt/{folder}/label'
+        image_outputs = os.path.join('synt', folder, 'image')
+        label_outputs = os.path.join('synt', folder, 'label')
 
         image_filename = os.path.basename(d['image_meta_dict']['filename_or_obj'][0])
         label_filename = os.path.basename(d['label_meta_dict']['filename_or_obj'][0])
 
         TumorSaver.save_nifti(image, image_affine_matrix, image_data_type, image_outputs, image_filename)
         TumorSaver.save_nifti(label, label_affine_matrix, label_data_type, label_outputs, label_filename)
+
